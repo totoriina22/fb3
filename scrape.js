2d25a11225e4c4d25c20534230e853aa821af59e
totoriina22 <103332207+totoriina22@users.noreply.github.com>
2022-04-11 04:27:46 +0200

    13eme

--- scrape.js
+++ scrape.js
@@ -31,7 +31,7 @@ async function login(username,password){
         // Remove the timeout
         timeout: 0,
       };
-  
+     
     
 
 /*--- auth login ---*/
@@ -44,49 +44,13 @@ async function login(username,password){
         document.querySelector(loginInputSelector).value = username
         document.querySelector(passwordInputSelector).value = password
     }, username, password, loginInputSelector, passwordInputSelector)
-   
-   
-    // press button login
+   // press button login
     await page.waitForTimeout(1000)
     const submitButtonSelector = 'button[type="submit"]'
     await page.waitForSelector(submitButtonSelector)
     await page.click(submitButtonSelector)
     await page.waitForTimeout(6000)
-    const cloudinary_options = { 
-        public_id : `newsshot/$fb` 
-      } ;
-      let shotResult = await page.screenshot({
-        fullPage: true
-      }).then((result) => {
-        console.log(`fb got some results.`);
-        return result;
-      }).catch(e => {
-        console.error(`fb Error in snapshotting news`, e);
-        return false;
-      });
-      // This step (Step 9): return cloudinaryPromise if screen
-      // capture is successful, or else return null
-      if (shotResult){
-        return cloudinaryPromise(shotResult, cloudinary_options);
-      }else{
-        return null;
-      }
-      
-    
-    
-    function cloudinaryPromise(shotResult, cloudinary_options){
-      return new Promise(function(res, rej){
-        cloudinary.v2.uploader.upload_stream(cloudinary_options,
-          function (error, cloudinary_result) {
-            if (error){
-              console.error('Upload to cloudinary failed: ', error);
-              rej(error);
-            }
-            res(cloudinary_result);
-          }
-        ).end(shotResult);
-      });
-    }
+
 /*--- user link ---*/
     await page.goto('https://www.facebook.com/messages')
     await page.waitForTimeout(6000)
